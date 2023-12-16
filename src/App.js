5441c405a44aed277ab662ad917b0fabf378bac9
Lakshan Maduka <lakshanmaduka24@gmail.com>
2023-12-17 03:27:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Content from './components/Content';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Content/>
+      <Footer />
+      
+    </>
+  );
+}
+
+export default App;
